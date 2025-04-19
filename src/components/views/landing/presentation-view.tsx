f8953fdf0dca0990a9e4528982589f985b351a59
Christian Mesina <chrmesina@icloud.com>
2025-04-18 22:27:44 -0400

    fix: remove font problem

--- src/components/views/landing/presentation-view.tsx
+++ src/components/views/landing/presentation-view.tsx
@@ -1,19 +1,12 @@
 "use client";
 import { Heading1 } from "@/components/ui/heading-1";
 
-import { Poppins } from "next/font/google";
 import { MultilangHello } from "./greeting-multi-len";
-
-export const pacifico = Poppins({
-  // subsets: [""],
-  weight: ["900"], // Solo tiene 400
-  display: "auto",
-});
 
 export const PresentationView = () => {
   return (
     <div>
-      <Heading1 className={`animate-gradient ${pacifico.className}`}>
+      <Heading1 className="animate-gradient">
         <MultilangHello /> soy{" "}
         <span className="animate-gradient-x bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-cyan-300 to-white bg-[length:300%_300%]">
           Christian Mesina
